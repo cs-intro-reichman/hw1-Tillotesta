2c05fe11a350a9ed48bc5dda349e800e81dde75d
Alberto Testa <alberto.testa@post.runi.ac.il>
2025-11-05 20:13:27 +0200

    completed Ascend

--- Ascend.java
+++ Ascend.java
@@ -2,7 +2,20 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		
+		int range = Integer.parseInt(args[0]);
+		
+
+		int a = (int) (Math.random() * range);
+		int b = (int) (Math.random() * range);
+		int c = (int) (Math.random() * range);
+
+		int min = Math.min(Math.min(a,b),c);
+		int max = Math.max(Math.max(a,b),c);
+		int middle = a + b + c - max - min;
+		
+		System.out.println(a+"  "+b+"  "+c);
+		System.out.println(min+"  "+middle+"  "+max);
 	}
 }
  
--- FVCalc.java
+++ FVCalc.java
@@ -5,29 +5,13 @@ public class FVCalc {
 		double current = Double.parseDouble(args[0]);
 		double interest = Double.parseDouble(args[1]);
 		double n = Double.parseDouble(args[2]);
-
-		if (current <= 0) {
-			System.err.println("Invalid number. Must be posistive.");
-			return;
-		}
-
-		if (interest < 0) {
-			System.err.println("Invalid number. Interest rate must be posistive.");
-			return;
-		}
-
-		if (n < 0) {
-			System.err.println("Invalid number. Number of years must be posistive.");
-			return;
-		}
 		
 
-		//Math.abs
-		//double newInterest = interest/100;
-		//double value = current * math.pow((1 + newinterest) ,n);
+		double newInterest = interest/100;
+		double value = current * Math.pow((1 + newInterest) ,n);
 		
-		//System.out.println((int) value);
-		
-		System.out.println(current+interest+n);
+		System.out.println((int) value);
+		System.out.println("After " + (int)n + " years, $" + (int)current + " saved at " + interest + "% will yield " + (int)value + ".");
+
 	}
 }
